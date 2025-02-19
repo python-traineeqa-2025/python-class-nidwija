a04f5c7cb76118af9c314730d27bbd6334e1d0e6
NidwijaB <nidwija.bhatta@cotiviti.com>
2025-02-19 15:47:26 +0000

    first class

---
+++ add.py
@@ -0,0 +1,4 @@
+a=int(input("enter value a"))
+b=int(input("enter value b"))
+
+print(a+b)
---
+++ first_class_basic.py
@@ -0,0 +1,35 @@
+import keyword
+
+print(keyword.kwlist)
+
+#variable name format
+snake_case=0
+camelCase=1
+
+
+i=1#int
+c='4' #string
+
+a=[1,2,35,6]#list
+
+t=(2,4)#tuple
+
+d={'a':'1'}
+
+#basic conditionals
+z=1
+if z==1:
+    print(z)
+elif z!=1:
+    print(z)
+else:
+    print(z)
+
+x=input("enter your value")
+# (a + b)  # Addition: 13
+# (a - b)  # Subtraction: 7
+# (a * b)  # Multiplication: 30
+# (a / b)  # Division: 3.3333
+# (a // b) # Floor Division: 3
+# (a % b)  # Modulus: 1 (Remainder)
+# (a ** b) # Exponentiation: 10^3 = 1000
