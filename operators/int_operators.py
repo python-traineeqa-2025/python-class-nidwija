fddcb2dbd8e092fc3b19bce290d3481e6afbf8c7
NidwijaB <nidwijab@gmail.com>
2025-02-24 12:15:04 +0545

    typecasting

--- operators/int_operators.py
+++ operators/int_operators.py
@@ -10,7 +10,7 @@ print(a // b)  # Floor Division (10 // 3 = 3) - removes decimal part
 print(a % b)  # Modulus (10 % 3 = 1) - remainder
 print(a ** b)  # Exponentiation (10^3 = 1000)
 
-# Integer Comparisons
+# comparison
 print(a > b)  # Greater than (True)
 print(a < b)  # Less than (False)
 print(a >= b)  # Greater than or equal to (True)
@@ -18,7 +18,7 @@ print(a <= b)  # Less than or equal to (False)
 print(a == b)  # Equality check (False)
 print(a != b)  # Not equal check (True)
 
-# Bitwise Operators
+# Bitwise
 print(a & b)  # AND (10 & 3 = 2)
 print(a | b)  # OR (10 | 3 = 11)
 print(a ^ b)  # XOR (10 ^ 3 = 9)
@@ -26,7 +26,7 @@ print(~a)  # Bitwise NOT (~10 = -11)
 print(a << 1)  # Left shift (10 << 1 = 20) - Multiply by 2
 print(a >> 1)  # Right shift (10 >> 1 = 5) - Divide by 2
 
-# Type Conversion
+# Typecasting
 c = "15"
 print(int(c) + 5)  # Convert string to integer and add (15 + 5 = 20)
 print(float(a))  # Convert integer to float (10.0)
