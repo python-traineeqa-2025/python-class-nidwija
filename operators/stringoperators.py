25500ea46b82ba33bbf931ac145f5a2a508adb15
NidwijaB <nidwijab@gmail.com>
2025-02-24 12:24:56 +0545

    updated

--- operators/stringoperators.py
+++ operators/stringoperators.py
@@ -1,12 +1,13 @@
-# Basic String Operations
-p = 'car'
-r = 'race'
+
+r = 'car'
+t="apple"
+p = 'race'
 
 print(p + r)  # Concatenation
 print(p * 4)  # Repetition
 print(p[1])  # Indexing
 print(r[1:3])  # Slicing [start:end]
-print(p[::-1])  # Reverse string
+print(t[::-1])  # Reverse string
 
 
 print(p.upper())  # Uppercase
@@ -15,11 +16,12 @@ print(p.title())  # Title Case
 
 # trim and replace
 a = ' hello '
-print(a.strip())  # Remove spaces from both ends
-print(r.replace('r', 'f'))  # Replace characters
+print(a)
+print(a.strip())
+print(p.replace('r', 'f'))
 
 # split/join
 b = 'hello my name'
-print(b.split(' '))  # Split into a list
+print(b.split(' '))
 o = ['hello', 'hi']
-print(','.join(o))  # Join with a separator
+print(','.join(o))
